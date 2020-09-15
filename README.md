0ef419534d1ed88e00b55d012666e983c3f941d0
Tygovanommen <tygovanommen@live.nl>
2020-09-15 13:44:33 +0200

    Added LM sensor

--- README.md
+++ README.md
@@ -6,6 +6,24 @@ An Arduino software to read the temperature and humidity sensor from the ESP8266
 * Board Manager URL: http://arduino.esp8266.com/stable/package_esp8266com_index.json
 * Tools board: NodeMCU 0.9 (ESP-12 Module)
 
-## Wiring setup
+## Chip setup
 
-<img src="setup.jpg">
+### DHT
+
+<img src="setup-DHT.jpg">
+
+| From (ESP)    | To (DHT chip) |
+| ------------- |---------------|
+| D1-8          | Signal 	|
+| G             | Vcc		| 
+| 3V	 	| Ground        |
+
+### LM
+
+<img src="setup-LM.jpg">
+
+| From (ESP)    | To (LM chip)  |
+| ------------- |---------------|
+| ABC           | DO     	|
+| G	        | Ground        | 
+| 3V	 	| Vcc		|
--- main/main.ino
+++ main/main.ino
@@ -1,27 +1,51 @@
 #include "DHT.h"
 
-#define DHTPIN 2
-#define DHTTYPE DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(2, DHT11);
 
 void setup(){
-  // Initiate Microsensor and LED light
+  // Initiate hardware
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   dht.begin();
 }
 
-void loop(){
-  // Flash light	
-  digitalWrite(13, LOW);   
-  delay(2000);
-  digitalWrite(13, HIGH);
+void loop(){ 
+  lightOFF();
+  delay(1000);
+  lightON();
+  delay(1000);
 
-  // Show temperature after light has flashed
+  //showDhtValues(); // If you want to see DHT values
+  showLmValues(); // If you want to see LM values
+}
+
+// Get air humisty and temprature values
+void showDhtValues() {
   Serial.print(F("Humidity: "));
   Serial.print(dht.readHumidity());
   Serial.print(F("%  Temperature: "));
   Serial.print(dht.readTemperature());
   Serial.println(F("°C "));
+}
+
+// Get soil moisture values
+void showLmValues() {
+   float sensor0 = analogRead(0);
+   Serial.print("Humidity: ");   
+   Serial.print(sensor0);
+     
+   float sensor0P = 100.00 - ( ( 100.00 * sensor0 ) / 1023.00 );
+   int sensorInt = (int) sensor0P;
+   Serial.print(" Humidity percent: ");   
+   Serial.println(sensorInt); 
+}
+
+// Turn LED light on
+void lightON() {
+    digitalWrite(13, HIGH);  
+}
+
+// Turn LED light off
+void lightOFF() {
+   digitalWrite(13, LOW);
 }
